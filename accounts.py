f91d86cb591bf211fb98dd8860d9fce709b34106
RSylla <103101146+RSylla@users.noreply.github.com>
2023-01-16 17:09:04 +0200

    Created functionality to open and close trades and to get dealId and dealReference number

--- accounts.py
+++ accounts.py
@@ -9,6 +9,7 @@ class Broker:
         self.__account_balance = self.__session["BALANCE"]
         self.__cst_token = self.__session["CST_TOKEN"]
         self.__security_token = self.__session["X_SECURITY_TOKEN"]
+        self.open_positions = []
 
     def __create_session(self):
         payload = json.dumps({
@@ -38,31 +39,73 @@ class Broker:
         return self.__account_balance
 
     def create_position(self, ticker: str, direction: str, qty: float, stop: float, profit: float):
-        payload = json.dumps({
+        position = {
             "epic": ticker,
             "direction": direction,
             "size": qty,
             "guaranteedStop": True,
             "stopLevel": stop,
             "profitLevel": profit
-        })
+        }
+        payload = json.dumps(position)
         headers = {
             'X-SECURITY-TOKEN': self.__security_token,
             'CST': self.__cst_token,
             'Content-Type': 'application/json'
         }
         self.connection.request("POST", "/api/v1/positions", payload, headers)
-        # res = self.connection.getresponse()
-        # data = res.read()
-        # print(data.decode("utf-8"))
+        res = self.connection.getresponse()
+        data = res.read()
+        dealReference = json.loads(data.decode("utf-8"))["dealReference"]
 
+        self.connection.request("GET", f"/api/v1/confirms/{dealReference}", "", headers)
+        res = self.connection.getresponse()
+        response = res.read()
+        data = json.loads(response.decode("utf-8"))
+        dealId = data["affectedDeals"][0]["dealId"]
+
+        position["dealId"] = dealId
+        self.open_positions.append(position)
+
+
+    def get_all_positions(self):
+        payload = ''
+        headers = {
+            'X-SECURITY-TOKEN': self.__security_token,
+            'CST': self.__cst_token
+        }
+        self.connection.request("GET", "/api/v1/positions", payload, headers)
+        res = self.connection.getresponse()
+        data = res.read()
+        print(data.decode("utf-8"))
+
+    def close_position(self, dealId: str):
+        payload = ''
+        headers = {
+            'X-SECURITY-TOKEN': self.__security_token,
+            'CST': self.__cst_token
+        }
+        self.connection.request("DELETE", f"/api/v1/positions/{dealId}", payload, headers)
+
+    def close_all_positions(self):
+        payload = ''
+        headers = {
+            'X-SECURITY-TOKEN': self.__security_token,
+            'CST': self.__cst_token
+        }
+        for position in self.open_positions:
+            dealId = position["dealId"]
+            self.connection.request("DELETE", f"/api/v1/positions/{dealId}", payload, headers)
+            self.open_positions.remove(position)
 
 
 obj = Broker()
 
-print(obj.get_account_balance())
-obj.create_position("SILVER", "BUY", 1.0, 22.0, 25.0)
+# print(obj.get_account_balance())
+# obj.create_position("SILVER", "BUY", 1.0, 22.0, 25.0)
+# obj.get_all_positions()
+# obj.close_position()
 
-
+obj.close_all_positions()
 
 
