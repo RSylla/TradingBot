5f16f21b1a9483065586868f5452d949fe3e2ba4
RSylla <103101146+RSylla@users.noreply.github.com>
2023-01-16 13:52:30 +0200

    Created functionality to create a new session, to get tokens and create position

--- accounts.py
+++ accounts.py
@@ -1,11 +1,68 @@
+import http.client
+import json
 
-
-class Broker():
+class Broker:
 
     def __init__(self):
-        pass
+        self.connection = http.client.HTTPSConnection("demo-api-capital.backend-capital.com")
+        self.__session = self.__create_session()
+        self.__account_balance = self.__session["BALANCE"]
+        self.__cst_token = self.__session["CST_TOKEN"]
+        self.__security_token = self.__session["X_SECURITY_TOKEN"]
 
-    def get_acc_balance(self):
-        pass
+    def __create_session(self):
+        payload = json.dumps({
+            "identifier": "raivo.sylla@gmail.com",
+            "password": "@S3ctumS3mpra"
+        })
+        headers = {
+            'X-CAP-API-KEY': '6NwR2oqK2tsHauJZ',
+            'Content-Type': 'application/json'
+        }
+        self.connection.request("POST", "/api/v1/session", payload, headers)
+        response = self.connection.getresponse()
+        read_response = response.read()
+        header = response.headers
+        data = json.loads(read_response.decode("utf-8"))
 
-    
+        cst_token = header['CST']
+        security_token = header['X-SECURITY-TOKEN']
+        account_balance = float(data["accountInfo"]["balance"])
+
+        output = {"CST_TOKEN": cst_token,
+                  "X_SECURITY_TOKEN": security_token,
+                  "BALANCE": account_balance}
+        return output
+
+    def get_account_balance(self):
+        return self.__account_balance
+
+    def create_position(self, ticker: str, direction: str, qty: float, stop: float, profit: float):
+        payload = json.dumps({
+            "epic": ticker,
+            "direction": direction,
+            "size": qty,
+            "guaranteedStop": True,
+            "stopLevel": stop,
+            "profitLevel": profit
+        })
+        headers = {
+            'X-SECURITY-TOKEN': self.__security_token,
+            'CST': self.__cst_token,
+            'Content-Type': 'application/json'
+        }
+        self.connection.request("POST", "/api/v1/positions", payload, headers)
+        # res = self.connection.getresponse()
+        # data = res.read()
+        # print(data.decode("utf-8"))
+
+
+
+obj = Broker()
+
+print(obj.get_account_balance())
+obj.create_position("SILVER", "BUY", 1.0, 22.0, 25.0)
+
+
+
+
